2e27f7969c600b740766dffa7773e2929bcb18d6
Big Chamel <ali10.imkamel@gmail.com>
2024-03-06 02:15:19 +0000

    updated urls and created router

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'patients', views.PatientProfileViewSet)
+router.register(r'dermatologists', views.DermatologistProfileViewSet)
 
 urlpatterns = [
-    path('hello-world/', views.hello_world, name='hello_world')
+    path('', include(router.urls)),
+    path('create_user/', views.create_user, name='create_user'),
 ]
