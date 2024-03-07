38f37fcdbd156c420a26c4caea4c62d07bfd4242
aliikamel <92301572+aliikamel@users.noreply.github.com>
2024-03-07 15:23:50 +0400

    removed hello_world view

--- model/urls.py
+++ model/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('hello-world/', views.hello_world, name='hello_world'),
     path('run-model/', views.run_model, name='run_model'),
     path('test-model/', views.test_model, name='test_model')
 ]
