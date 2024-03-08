fcee6a9dc54a32b725a01e3a6f82f855b14746d4
aliikamel <92301572+aliikamel@users.noreply.github.com>
2024-03-08 04:30:34 +0400

    improved styling

--- frontend/src/components/PasiUvbForm.jsx
+++ frontend/src/components/PasiUvbForm.jsx
@@ -99,118 +99,122 @@ function PasiUvbForm() {
   const initialUvbCount = 16; // Example initial count for UVB doses
 
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <div className="mt-4">
-          <label
-            htmlFor="pasiPreTreatment"
-            className="block text-sm font-medium text-gray-700 dark:text-gray-100 dark:text-gray-100"
-          >
-            PASI_PRE_TREATMENT
-          </label>
-          <input
-            type="number"
-            id="pasiPreTreatment"
-            value={pasiPreTreatment}
-            onChange={(e) => setPasiPreTreatment(e.target.value)}
-            className="mt-1 block w-80 rounded-md border-gray-300 shadow-sm bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 sm:text-sm"
-          />
-        </div>
-        {/* PASI Weeks Inputs */}
-        <div className="grid grid-cols-4 gap-4">
-          {pasiWeeks.map((week, index) => (
-            <div key={index} className="relative space-y-2">
+    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div className="flex w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-lg xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <div className="mt-4">
               <label
-                htmlFor={week.week}
+                htmlFor="pasiPreTreatment"
                 className="block text-sm font-medium text-gray-700 dark:text-gray-100 dark:text-gray-100"
               >
-                {week.week}
+                PASI Pre-Treatment
               </label>
               <input
                 type="number"
-                id={week.week}
-                value={week.value}
-                onChange={(e) => handlePasiChange(index, e.target.value)}
-                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 sm:text-sm"
+                id="pasiPreTreatment"
+                value={pasiPreTreatment}
+                onChange={(e) => setPasiPreTreatment(e.target.value)}
+                className="mt-1 block w-80 rounded-md border-gray-300 shadow-sm bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 sm:text-sm"
               />
-              {index >= initialPasiCount && ( // Only render remove button for additional inputs
-                <button
-                  type="button"
-                  onClick={() => removePasiWeek(index)}
-                  className="absolute top-0 right-0 mt-3 mr-2 text-sm font-medium text-white bg-red-600 hover:bg-red-700 rounded-full h-6 w-6 flex items-center justify-center"
-                >
-                  &times; {/* X symbol */}
-                </button>
-              )}
             </div>
-          ))}
-        </div>
-        <button
-          type="button"
-          onClick={addPasiWeek}
-          className="mt-2 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
-        >
-          Add PASI Week
-        </button>
-        {/* PASI_END_TREATMENT Input */}
-        <div className="mt-4">
-          <label
-            htmlFor="pasiEndTreatment"
-            className="block text-sm font-medium text-gray-700 dark:text-gray-100"
-          >
-            PASI_END_TREATMENT
-          </label>
-          <input
-            type="number"
-            id="pasiEndTreatment"
-            value={pasiEndTreatment}
-            onChange={(e) => setPasiEndTreatment(e.target.value)}
-            className="mt-1 block w-full rounded-md border-gray-300 shadow-sm bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
-          />
-        </div>
-        {/* UVB Doses Inputs */}
-        <div className="grid grid-cols-4 gap-4">
-          {uvbDoses.map((dose, index) => (
-            <div key={index} className="relative space-y-2">
+            {/* PASI Weeks Inputs */}
+            <div className="grid grid-cols-4 gap-4">
+              {pasiWeeks.map((week, index) => (
+                <div key={index} className="relative space-y-2">
+                  <label
+                    htmlFor={week.week}
+                    className="block text-sm font-medium text-gray-700 dark:text-gray-100 dark:text-gray-100"
+                  >
+                    {`Week ${Number(index) + 1}`}
+                  </label>
+                  <input
+                    type="number"
+                    id={week.week}
+                    value={week.value}
+                    onChange={(e) => handlePasiChange(index, e.target.value)}
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 sm:text-sm"
+                  />
+                  {index >= initialPasiCount && ( // Only render remove button for additional inputs
+                    <button
+                      type="button"
+                      onClick={() => removePasiWeek(index)}
+                      className="absolute top-0 right-0 mt-3 mr-2 text-sm font-medium text-white bg-red-600 hover:bg-red-700 rounded-full h-6 w-6 flex items-center justify-center"
+                    >
+                      &times; {/* X symbol */}
+                    </button>
+                  )}
+                </div>
+              ))}
+            </div>
+            <button
+              type="button"
+              onClick={addPasiWeek}
+              className="mt-2 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
+            >
+              Add PASI Week
+            </button>
+            {/* PASI_END_TREATMENT Input */}
+            <div className="mt-4">
               <label
-                htmlFor={dose.dose}
+                htmlFor="pasiEndTreatment"
                 className="block text-sm font-medium text-gray-700 dark:text-gray-100"
               >
-                {dose.dose}
+                PASI End of Treatment
               </label>
               <input
                 type="number"
-                id={dose.dose}
-                value={dose.value}
-                onChange={(e) => handleUvbChange(index, e.target.value)}
+                id="pasiEndTreatment"
+                value={pasiEndTreatment}
+                onChange={(e) => setPasiEndTreatment(e.target.value)}
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
               />
-              {index >= initialUvbCount && ( // Only render remove button for additional inputs
-                <button
-                  type="button"
-                  onClick={() => removeUvbDose(index)}
-                  className="absolute top-1/3 right-2 mt-3 mr-2 text-sm font-medium text-white bg-red-600 hover:bg-red-700 rounded-full h-6 w-6 flex items-center justify-center"
-                >
-                  &times; {/* X symbol */}
-                </button>
-              )}
             </div>
-          ))}
+            {/* UVB Doses Inputs */}
+            <div className="grid grid-cols-4 gap-4">
+              {uvbDoses.map((dose, index) => (
+                <div key={index} className="relative space-y-2">
+                  <label
+                    htmlFor={dose.dose}
+                    className="block text-sm font-medium text-gray-700 dark:text-gray-100"
+                  >
+                    {`Dose ${Number(index) + 1}`}
+                  </label>
+                  <input
+                    type="number"
+                    id={dose.dose}
+                    value={dose.value}
+                    onChange={(e) => handleUvbChange(index, e.target.value)}
+                    className="mt-1 block w-full rounded-md border-gray-300 shadow-sm bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white focus:border-blue-500 focus:ring-blue-500 sm:text-sm"
+                  />
+                  {index >= initialUvbCount && ( // Only render remove button for additional inputs
+                    <button
+                      type="button"
+                      onClick={() => removeUvbDose(index)}
+                      className="absolute top-1/3 right-2 mt-3 mr-2 text-sm font-medium text-white bg-red-600 hover:bg-red-700 rounded-full h-6 w-6 flex items-center justify-center"
+                    >
+                      &times; {/* X symbol */}
+                    </button>
+                  )}
+                </div>
+              ))}
+            </div>
+            <button
+              type="button"
+              onClick={addUvbDose}
+              className="mt-2 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
+            >
+              Add UVB Dose
+            </button>
+            <button
+              type="submit" // This ensures the form gets submitted
+              className="mt-4 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-500 hover:bg-green-600"
+            >
+              Submit
+            </button>
+          </form>
         </div>
-        <button
-          type="button"
-          onClick={addUvbDose}
-          className="mt-2 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
-        >
-          Add UVB Dose
-        </button>
-        <button
-          type="submit" // This ensures the form gets submitted
-          className="mt-4 py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-500 hover:bg-green-600"
-        >
-          Submit
-        </button>
-      </form>
+      </div>
     </div>
   );
 }
