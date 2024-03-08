f1ec8a05a80d92475cba27397a0cf315cf0bc34d
aliikamel <92301572+aliikamel@users.noreply.github.com>
2024-03-08 04:29:53 +0400

    updated logout()

--- frontend/src/utils/logout.jsx
+++ frontend/src/utils/logout.jsx
@@ -1,5 +1,6 @@
 export const logout = () => {
   localStorage.removeItem("accessToken");
   localStorage.removeItem("refreshToken");
+  localStorage.removeItem("userRole");
   // Redirect to login page or update the state to reflect the logout
 };
