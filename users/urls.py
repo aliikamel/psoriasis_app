cf266833949968ef4ee495a3140e26cd1c26c6a4
aliikamel <92301572+aliikamel@users.noreply.github.com>
2024-03-13 03:18:23 +0400

    Added 3 patient API endpoints

--- users/urls.py
+++ users/urls.py
@@ -11,4 +11,7 @@ router.register(r'users', views.CustomUserViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('create-user/', views.create_user, name='create_user'),
+    path('add-patient/', views.create_patient_dermatologist_relationship, name='create_patient_dermatologist_relationship'),
+    path('get-patients-managed/', views.get_patients_managed, name='get_patients_managed'),
+    path('get-patient-details/', views.get_patient_details_by_id, name='get_patients_details_by_id')
 ]
--- users/views.py
+++ users/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status, viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import CustomUser, PatientProfile, DermatologistProfile
+from .models import CustomUser, PatientProfile, DermatologistProfile, DermatologistPatientRelationship
 from .serializers import (CustomUserSerializer, PatientProfileSerializer,
-                          DermatologistProfileSerializer, UserCreateSerializer)
+                          DermatologistProfileSerializer, UserCreateSerializer,
+                          DermatologistPatientRelationshipSerializer)
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.core.exceptions import ObjectDoesNotExist
 
 
 @api_view(['POST'])
@@ -33,6 +35,56 @@ def create_user(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+@api_view(['POST'])
+def create_patient_dermatologist_relationship(request):
+    serializer = DermatologistPatientRelationshipSerializer(data=request.data)
+
+    if serializer.is_valid():
+        relationship = serializer.save()
+
+        response_data = {
+            'id': relationship.id,
+            'dermatologist': relationship.dermatologist.id,
+            'patient': relationship.patient.id,
+        }
+
+        return Response(response_data, status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def get_patients_managed(request):
+    # Get the dermatologist_id from query parameters
+    dermatologist_id = request.query_params.get('dermatologist_id')
+
+    # Filter the queryset based on the dermatologist_id, if provided
+    if dermatologist_id is not None:
+        relationships = DermatologistPatientRelationship.objects.filter(dermatologist_id=dermatologist_id)
+        patient_ids = relationships.values_list('patient_id', flat=True)
+
+        # Fetching patient profiles based on those IDs
+        patients = CustomUser.objects.filter(id__in=patient_ids)
+        serializer = CustomUserSerializer(patients, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    else:
+        return Response({"error": "Dermatologist ID is required."}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def get_patient_details_by_id(request):
+    # Get the dermatologist_id from query parameters
+    patient_id = request.query_params.get('patient_id')
+
+    if patient_id is not None:
+        # Assuming `user` is a ForeignKey in PatientProfile pointing to the user model
+        patient_profile = PatientProfile.objects.get(user_id=patient_id)
+        serializer = PatientProfileSerializer(patient_profile)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    else:
+        return Response({"error": "Patient ID is required."}, status=status.HTTP_400_BAD_REQUEST)
+
+
 class CustomUserViewSet(viewsets.ModelViewSet):
     """
     A viewset for viewing and editing patient profiles.
@@ -55,4 +107,3 @@ class DermatologistProfileViewSet(viewsets.ModelViewSet):
     """
     serializer_class = DermatologistProfileSerializer
     queryset = DermatologistProfile.objects.all()
-
