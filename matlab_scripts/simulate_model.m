27e43e40950e098e154e8f84d8591e5711e2feec
aliikamel <92301572+aliikamel@users.noreply.github.com>
2024-03-28 19:22:09 +0400

    updated simulate_model.m

--- matlab_scripts/simulate_model.m
+++ matlab_scripts/simulate_model.m
@@ -5,8 +5,6 @@ function sim_struct = simulate_model(best_uv_eff, model_path, uvb_doses, time_do
     % Set the best UVB efficacy parameter
     model = sbml_set_parameter_value(model, 'uv_eff', best_uv_eff);
 
-    % Set simulation stop time
-    %model.getconfigset.StopTime = 735;
     % model simulation upper time bound
     model.getconfigset.StopTime = time_doses(end)*2;
 
@@ -24,10 +22,14 @@ function sim_struct = simulate_model(best_uv_eff, model_path, uvb_doses, time_do
     % Simulate the model
     sim_data = sbiosimulate(model);
 
-    % Display sizes of sim_data.Data and sim_data.DataNames
-    % disp(['Size of sim_data.Data: ', mat2str(size(sim_data.Data))]);
-    % disp(['Length of sim_data.DataNames: ', num2str(length(sim_data.DataNames))]);
-
     % Create a scalar struct with two fields
     sim_struct = struct('Data', sim_data.Data, 'DataNames', {sim_data.DataNames}, 'Time', sim_data.Time);
 end
+
+
+
+    % Set simulation stop time
+    %model.getconfigset.StopTime = 735;
+    % Display sizes of sim_data.Data and sim_data.DataNames
+    % disp(['Size of sim_data.Data: ', mat2str(size(sim_data.Data))]);
+    % disp(['Length of sim_data.DataNames: ', num2str(length(sim_data.DataNames))]);
