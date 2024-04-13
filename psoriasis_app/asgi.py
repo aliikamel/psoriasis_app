2bd0cce1ebaf61d141f8ef22d3cbca19582ba7b2
aliikamel <92301572+aliikamel@users.noreply.github.com>
2024-04-13 20:28:13 +0400

    Setup ASGI Config for project

--- psoriasis_app/asgi.py
+++ psoriasis_app/asgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
+from model.consumers import SimulationConsumer
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'psoriasis_app.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),  # Django's ASGI application to handle traditional HTTP requests
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            path("ws/simulate/", SimulationConsumer.as_asgi()),
+        ])
+    ),
+})
