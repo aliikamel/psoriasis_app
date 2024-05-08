44dc45d6100b6b9c84a4ae140d815d54f95315bc
Big Chamel <ali10.imkamel@gmail.com>
2024-05-08 23:45:49 +0100

    home page

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useAuth } from "../context/AuthContext";
 
 function Home() {
-  return <div></div>;
+  const { isAuthenticated, role } = useAuth();
+  return (
+    <div className="flex flex-col items-center justify-center h-full">
+      <h1 className="text-9xl text-center font-bold leading-tight tracking-tight text-blue-700">
+        Hello
+      </h1>
+      <h1 className="text-6xl text-center font-bold leading-tight tracking-tight text-gray-900 dark:text-white">
+        {isAuthenticated &&
+          role === "dermatologist" &&
+          `${localStorage.getItem("firstName")} ${localStorage.getItem("lastName")}`}
+        {isAuthenticated &&
+          role === "patient" &&
+          localStorage.getItem("firstName")}
+      </h1>
+    </div>
+  );
 }
 
 export default Home;
