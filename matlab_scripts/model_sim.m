b0411fd475c6817aa1dff253dbe6290321419700
Big Chamel <ali10.imkamel@gmail.com>
2024-05-12 00:15:09 +0100

    Added EDA Directory

--- matlab_scripts/model_sim.m
+++
@@ -1,7 +0,0 @@
-function [res] = model_sim(sbml,time_bound)
-%SBML_SIM_AND_PLOT Summary of this function goes here
-%   Detailed explanation goes here
-    sbml.getconfigset.StopTime = time_bound;
-    res = sbiosimulate(sbml);
-end
-
