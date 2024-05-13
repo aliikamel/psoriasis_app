d09726e16ad70f448ba37f374f8f2a848e10b8b3
Big Chamel <ali10.imkamel@gmail.com>
2024-05-13 15:59:10 +0100

    update settings

--- psoriasis_app/settings.py
+++ psoriasis_app/settings.py
@@ -89,13 +89,17 @@ WSGI_APPLICATION = 'psoriasis_app.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'psoriasis_db',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'postgres',
+    #     'HOST':'localhost',
+    #     'PORT': '5432',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'psoriasis_db',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST':'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
